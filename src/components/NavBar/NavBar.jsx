9a3211e49d273782d3433e3da340a8d25182da6a
RazorHollow <rob@razorhollow.com>
2022-08-19 17:03:35 -0400

    base css and navbar setup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
-      {user ?
+      <header className='App-header'>
+        {user ?
         <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
-        </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
+          Welcome, {user.name}
+          <Link to="/">Posts</Link>
+          <Link to="" onClick={handleLogout}>LOG OUT</Link>
+          <Link to="/changePassword">Change Password</Link>
+          </nav>
+        :
+          <nav>
+            <Link to="/login">Log In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </nav>
+        }
+      </header>
     </>
   )
 }
